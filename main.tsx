c26ac2843357280bd8cc94b9372e5ffc3d0f6aff
Swastik Chandel <swastikchandel@Swastiks-MacBook-Air.local>
2026-05-07 20:29:03 +0530

    Fixed static Vite deployment

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "src/styles.css";
-import Index from "./routes/index";
+import Index from "./src/routes/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
