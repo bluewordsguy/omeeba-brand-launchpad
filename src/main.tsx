e7bb77428b7e4bd7ef450e0e8283c133dcc3d0e9
Swastik Chandel <swastikchandel@Swastiks-MacBook-Air.local>
2026-05-07 20:37:45 +0530

    Fixed app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "src/styles.css";
-import Index from "./src/routes/index";
+import "./styles.css";
+import Index from "./routes/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
